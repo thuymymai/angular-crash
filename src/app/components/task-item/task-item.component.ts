19d69d3aebbbfd0df7b66b13d7f7f4998f815f49
Amy Mai <My.Mai@metropolia.fi>
2022-05-17 22:32:09 +0300

    added task and task item with hardcoded data

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,19 @@
+import { TASKS } from './../../mock-tasks';
+import { Component, OnInit, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input()
+  task!: Task;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
